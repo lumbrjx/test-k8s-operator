1d4e6b81f9ba3676d2bdd75384abb443ea29799e
CLOG9 <cakipan677@gmail.com>
2024-06-06 15:55:39 +0100

    Update CRD

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
-	"os"
-	"os/signal"
+	"log"
 	"path/filepath"
-	"syscall"
-	"time"
 
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/client-go/kubernetes"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
@@ -22,67 +22,68 @@ func main() {
 	var kubeconfig string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = filepath.Join(home, ".kube", "config")
-	} else {
-		kubeconfig = ""
 	}
-	flag.StringVar(
-		&kubeconfig,
-		"kubeconfig",
-		kubeconfig,
-		"(optional) absolute path to the kubeconfig file",
-	)
-	flag.Parse()
 
-	var config *rest.Config
-	var err error
-
-	if kubeconfig != "" {
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-	} else {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		fmt.Println("Falling back to in-cluster config")
 		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 
+	dynClient, err := dynamic.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
+	hellowrld := schema.GroupVersionResource{
+		Group:    "hlw.io",
+		Version:  "v1",
+		Resource: "hellowrlds",
 	}
 
-	// Pod informer
-	watchlist := cache.NewListWatchFromClient(
-		clientset.CoreV1().RESTClient(),
-		"pods",
-		v1.NamespaceDefault,
-		fields.Everything(),
-	)
-	_, controller := cache.NewInformer(
-		watchlist,
-		&v1.Pod{},
-		time.Second*0,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				fmt.Println("Hello World: Pod added")
+	informer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return dynClient.Resource(hellowrld).
+					Namespace("").
+					List(context.TODO(), options)
 			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				fmt.Println("Pod updated")
-			},
-			DeleteFunc: func(obj interface{}) {
-				fmt.Println("Pod deleted")
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return dynClient.Resource(hellowrld).
+					Namespace("").
+					Watch(context.TODO(), options)
 			},
 		},
+		&unstructured.Unstructured{},
+		0, // Skip resync
+		cache.Indexers{},
 	)
 
-	fmt.Println("created")
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			log.Println("Add event detected:", obj)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			log.Println("Update event detected:", newObj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			log.Println("Delete event detected:", obj)
+		},
+	})
+
 	stop := make(chan struct{})
 	defer close(stop)
 
-	go controller.Run(stop)
+	go informer.Run(stop)
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	<-signalChan
-	fmt.Println("Shutting down gracefully...")
+	if !cache.WaitForCacheSync(stop, informer.HasSynced) {
+		panic("Timeout waiting for cache sync")
+	}
+
+	fmt.Println("Custom Resource Controller started successfully")
+
+	<-stop
 }
